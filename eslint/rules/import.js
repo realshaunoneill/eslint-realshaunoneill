6776c1b61a6a15234290063d23da3b1e391c3e9b
Shaun O'Neill <shaunoneill7@gmail.com>
2024-02-10 01:56:24 +0000

    Initial style guide config

---
+++ eslint/rules/import.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: ['import'],
+  rules: {
+    'import/default': 2,
+    'import/export': 2,
+    'import/first': 2,
+    'import/named': 2,
+    'import/namespace': 2,
+    'import/newline-after-import': 2,
+    'import/no-duplicates': 2,
+    'import/no-extraneous-dependencies': 2,
+    'import/no-self-import': 2,
+  },
+};
---
+++ eslint/rules/node.js
@@ -0,0 +1,8 @@
+module.exports = {
+  rules: {
+    'linebreak-style': [1, 'unix'],
+    'lines-around-directive': ['error', 'always'],
+    'no-irregular-whitespace': 1,
+    'no-trailing-spaces': 1,
+  },
+};
