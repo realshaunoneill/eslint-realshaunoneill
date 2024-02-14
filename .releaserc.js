31d2617a8e2fb81de35d04561b2a22604e007826
Shaun O'Neill <shaunoneill7@gmail.com>
2024-02-14 01:42:51 +0000

    fix: repo url

---
+++ .releaserc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  branches: [
+    'master',
+  ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        // eslint-disable-next-line no-template-curly-in-string -- required by `semantic-release`.
+        message: 'release: ${nextRelease.version}',
+      },
+    ],
+  ],
+};
